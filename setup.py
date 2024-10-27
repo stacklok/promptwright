aebe823f753238831a10cc064eb604d82c614689
Luke Hinds <luke@stacklok.com>
2024-10-27 08:02:16 +0000

    Prepare 013 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setup(
     name="promptwright",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "certifi==2024.8.30",
